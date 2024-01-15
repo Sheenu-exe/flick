2b57bcbee6d734cc45c0c83c3b324fbd2532a0cf
Sachin Parihar <130788900+Sheenu-exe@users.noreply.github.com>
2024-01-15 18:12:51 +0530

    Update AppWrapper.jsx

--- src/components/AppWrapper.jsx
+++ src/components/AppWrapper.jsx
@@ -47,7 +47,7 @@ l100 27 652 6 c641 6 652 6 680 27 94 70 98 191 7 260 l-37 29 -636 1 c-350 1
       <div className="dropdown">
   <div tabIndex={0} role="button" className="btn m-1">
     Theme
-    <svg width="12px" height="12px" className="h-2 w-2 fill-current opacity-60 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048"><path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path></svg>
+    
   </div>
   <ul tabIndex={0} className="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52">
     <li><input type="radio" name="theme-dropdown" className="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Default" value="default"/></li>
